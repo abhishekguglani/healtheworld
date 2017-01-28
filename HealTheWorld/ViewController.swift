07c7d8ff24a5b7f64903953af647439ab6bbab2d
abhishek guglani <abhishekguglani@abhisheks-mbp.hsd1.ca.comcast.net>
2017-01-27 21:46:40 -0800

    Initial Commit

---
+++ HealTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealTheWorld
+//
+//  Created by abhishek guglani on 1/27/17.
+//  Copyright © 2017 abhishek guglani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
